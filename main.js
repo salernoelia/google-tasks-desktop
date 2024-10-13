a2b523102c9c45b7b1b6e7f0cdd320a951f599db
salernoelia <putopet@gmail.com>
2024-10-14 00:14:02 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    show: false,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+  win.maximize();
+  win.show();
+  // Load the Google Tasks URL.
+  win.loadURL("https://calendar.google.com/calendar/u/0/r/tasks");
+}
+
+// Called when Electron has finished initialization.
+app.whenReady().then(createWindow);
+
+// Quit the app when all windows are closed (except on macOS).
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+// Re-create a window when the app is activated (macOS).
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
